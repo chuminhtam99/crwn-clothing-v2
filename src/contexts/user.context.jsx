6a118b7a6d8c4c1c54b744c6ce91fa2d63549dfb
chuminhtam99 <68324414+chuminhtam99@users.noreply.github.com>
2024-01-04 02:35:32 +0700

    Update user.context.jsx

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -15,6 +15,10 @@ export const UserProvider = ({ children }) => {
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
+    // 2. ta cho 1 cái component did mount (tuy là context nhưng vẫn chạy như các component khác). nếu onAuthStateChangedListener((user) => {
+      // if (user) {
+      //   log(user);
+      // } thì ta vẫn sẽ nhận đc user cũ, dù nó có sign in hay out, vì fire base luôn giữ kết nối vs cái đã đăng nhập và luôn mark nó là authentication => thêm signOutUser tại firebase.util .... lằng nhằng => túm lại là nhờ thằng này (của firebase) sẽ nghe ngóng mọi động tĩnh in/out, dồn lại 1 chỗ này, ko cần phải dùng useContext mỗi file 1 ít như bài trc nữa! 
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
         createUserDocumentFromAuth(user);
