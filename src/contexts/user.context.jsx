5f5615729f5b8ed8dc603a5e2bd88e9d107cb8b0
chuminhtam99 <68324414+chuminhtam99@users.noreply.github.com>
2024-01-04 01:25:56 +0700

    Update user.context.jsx

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -10,5 +10,5 @@ export const UserProvider = ({ children }) => {
   const value = { currentUser, setCurrentUser };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-  // 1. viết code như trên, mục đích là cho những node thuộc children có đc truyền access context
+  // 1. viết code như trên, mục đích là cho những node thuộc children có đc truyền access context, xem tiếp tại index.js
 };
