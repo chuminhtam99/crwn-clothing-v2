9ca71edd2f7178869579afb8bb17bd9141138aaa
chuminhtam99 <68324414+chuminhtam99@users.noreply.github.com>
2024-01-04 22:49:05 +0700

    Update products.context.jsx

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -3,14 +3,14 @@ import { createContext, useState, useEffect } from 'react';
 import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
 
 import SHOP_DATA from '../shop-data.js';
-
+// 2. chỉnh
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-
+// 2. chỉnh
   // useEffect(() => {
   //   addCollectionAndDocuments('collections', SHOP_DATA);
   // }, []);
