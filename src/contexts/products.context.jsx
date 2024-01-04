ef80b81a743511d95968a3c3d17e7fa04e5e17f4
chuminhtam99 <68324414+chuminhtam99@users.noreply.github.com>
2024-01-04 10:16:18 +0700

    Update products.context.jsx

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -9,6 +9,7 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState(PRODUCTS);
   const value = { products };
+  // 3. ta cho 1 data từ file json vào làm products của context
   return (
     <ProductsContext.Provider value={value}>
       {children}
